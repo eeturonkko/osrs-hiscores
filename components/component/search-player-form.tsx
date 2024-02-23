76993daed84d5f27023400a6c31678c0c3b6956e
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-02-23 21:46:42 +0200

    Update search form

--- components/component/search-player-form.tsx
+++ components/component/search-player-form.tsx
@@ -6,6 +6,13 @@ import { Label } from "@/components/ui/label";
 import { getNonIronmanPlayerInformation } from "@/actions/playerInformationActions";
 
 function SearchPlayerForm() {
+  async function handleSearch(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    await getNonIronmanPlayerInformation(formData);
+    form.reset();
+  }
   return (
     <Card className="bg-[#1e1f21]">
       <CardHeader>
@@ -16,10 +23,7 @@ function SearchPlayerForm() {
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <form
-          className="flex flex-col gap-2"
-          action={getNonIronmanPlayerInformation}
-        >
+        <form className="flex flex-col gap-2" onSubmit={handleSearch}>
           <Input id="name" name="name" placeholder="Search" />
           <Button type="submit" className="bg-[#333533] text-white">
             Search
