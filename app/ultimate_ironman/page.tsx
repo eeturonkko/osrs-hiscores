f17d11e11bd196bf5e2e64a61609c0bc4d0534ba
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-02-26 12:30:44 +0200

    add ironman & ultimate_ironman routes

---
+++ app/ultimate_ironman/page.tsx
@@ -0,0 +1,8 @@
+import { Hiscore } from "@/components/component/hiscore";
+export default function HardcoreIronmanPage() {
+  return (
+    <main className="container max-h-screen">
+      <Hiscore />
+    </main>
+  );
+}
