51cd91fd4d6dc14902e7e0364658d8a8b878c12f
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-02-26 12:34:45 +0200

    Refactor header component to include links in select dropdown

--- components/component/header.tsx
+++ components/component/header.tsx
@@ -7,21 +7,35 @@ import {
   Select,
 } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 function Header() {
   return (
     <header>
       <nav className="flex justify-between items-center w-full max-w-6xl px-4">
         <div className="flex space-x-4">
-          <Button className="bg-[#333533] text-white">Hiscores</Button>
+          <Button className="bg-[#333533] text-white">
+            <Link href="/">Hiscores</Link>
+          </Button>
           <Select>
             <SelectTrigger id="ironman" className="bg-[#333533] text-white">
               <SelectValue placeholder="Ironman" />
             </SelectTrigger>
-            <SelectContent position="popper">
-              <SelectItem value="ironman">Ironman</SelectItem>
-              <SelectItem value="ultimate">Ultimate</SelectItem>
-              <SelectItem value="hardcore">Hardcore</SelectItem>
+            <SelectContent
+              className="bg-[#333533] border-none"
+              position="popper"
+            >
+              <div className="flex flex-col gap-2 ">
+                <Button className="bg-[#333533] text-white">
+                  <Link href="/ironman">Ironman</Link>
+                </Button>
+                <Button className="bg-[#333533] text-white">
+                  <Link href="/ultimate_ironman">Ultimate Ironman</Link>
+                </Button>
+                <Button className="bg-[#333533] text-white">
+                  <Link href="/hardcore_ironman">Hardcore Ironman</Link>
+                </Button>
+              </div>
             </SelectContent>
           </Select>
 
